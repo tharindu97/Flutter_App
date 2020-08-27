6050871728da549e5d30f5bd2bafece7f1d9846c
tharindu97 <tharindukavishna@gmail.com>
2020-08-27 10:47:12 +0530

    jsonplaceholder_api_get_data

---
+++ apicall/lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Future<String> getData() async{
+
+    http.Response response = await http.get(
+      Uri.encodeFull("https://jsonplaceholder.typicode.com/posts"),
+      headers: {
+        "Accept": "application/json"
+      }
+    );
+
+    List data = json.decode(response.body);
+    print(data[2]["title"]);
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('Get data'),
+          onPressed: getData
+        ),
+      ),
+    );
+  }
+}
